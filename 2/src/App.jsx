20de10ea6c4695515cd650a0aadd1f792ba3b4b4
riteg21 <santalov445@gmail.com>
2025-08-27 01:20:30 +0400

    init-of-react

---
+++ 2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="hello">
+      <span>This is App component</span>
+    </div>
+  );
+};
+
+export default App;
